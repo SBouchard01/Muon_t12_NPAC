9ac40685c38b6836dea181226e25f4cd8cd4cd35
SBouchard01 <simonbouchard47@gmail.com>
2023-09-25 11:34:44 +0200

    Removed rebin

--- muon_software/utils.py
+++ muon_software/utils.py
@@ -1,41 +1,5 @@
 import numpy as np
 
-def rebin(x, height, factor):
-    """
-    Rebins an histogram in n_bins
-
-    Parameters
-    ----------
-    x : Array-like
-        The positions of the bins. 
-    height : Array-like
-        The values of the histogram to rebin, of dimension (1, N)
-    factor : int
-        The factor of division (i.e. how many bins are merged into one)
-    
-    Returns
-    -------
-    hist : Array-like
-        The rebinned histogram, of dimension (1, N') with N' = ceil(N/factor)
-    """
-    
-    # Get the number of bins
-    n_bins = len(x)
-    
-    # Get the number of bins in the rebinned histogram
-    n_bins_rebinned = int(np.ceil(n_bins/factor))
-    
-    # Create the rebinned histogram
-    hist = np.zeros((n_bins_rebinned, 2))
-    
-    # Fill the rebinned histogram
-    for i in range(n_bins_rebinned):
-        hist[i, 0] = np.mean(x[i*factor:(i+1)*factor])
-        hist[i, 1] = np.sum(height[i*factor:(i+1)*factor])
-        
-    return hist
-
-
 def chi2(y, y_fit, sigma):
     return np.sum(((y-y_fit)/sigma)**2)
 
